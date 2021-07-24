0d4f400e24df67039ed67f3cdcb81c2a13e751a7
ggoraa <ggoraa1029@gmail.com>
2021-07-25 00:18:46 +0300

    small refactor of buildApi

--- src/buildApi.ts
+++ src/buildApi.ts
@@ -2,16 +2,15 @@ import * as vscode from 'vscode';
 import * as bombadil from '@sgarciac/bombadil';
 import * as fs from 'fs';
 import * as child_process from 'child_process';
+import { getParsedConfig } from './configParser';
 
 export function buildProject() {
     let workspaceUri = vscode.workspace.workspaceFolders;
     if (workspaceUri === undefined) {
         vscode.window.showErrorMessage('No workspaces were found, can\'t execute this command.');
     } else {
-        let rawConfig = fs.readFileSync(workspaceUri[0].uri.path + '/kumitateru.toml', 'utf-8');
-        let reader = new bombadil.TomlReader;
-        reader.readToml(rawConfig);
-        vscode.window.showQuickPick(reader.result["package_meta"]["devices"], { title: 'Select the target device', canPickMany: false }).then((value) => {
+        let devicesArray = getParsedConfig(fs.readFileSync(workspaceUri[0].uri.path + '/kumitateru.toml', 'utf-8'))["package_meta"]["devices"];
+        vscode.window.showQuickPick(devicesArray, { title: 'Select the target device', canPickMany: false }).then((value) => {
             if (value !== undefined) {
                 let statusBarMessage = vscode.window.setStatusBarMessage('Building the project...');
                 let process = child_process.exec(workspaceUri[0].uri.path + '/kmtr build --target ' + value, { cwd: workspaceUri[0].uri.path });
@@ -33,10 +32,8 @@ export function runProject() {
     if (workspaceUri === undefined) {
         vscode.window.showErrorMessage('No workspaces were found, can\'t execute this command.');
     } else {
-        let rawConfig = fs.readFileSync(workspaceUri[0].uri.path + '/kumitateru.toml', 'utf-8');
-        let reader = new bombadil.TomlReader;
-        reader.readToml(rawConfig);
-        vscode.window.showQuickPick(reader.result["package_meta"]["devices"], { title: 'Select the target device', canPickMany: false }).then((value) => {
+        let devicesArray = getParsedConfig(fs.readFileSync(workspaceUri[0].uri.path + '/kumitateru.toml', 'utf-8'))["package_meta"]["devices"];
+        vscode.window.showQuickPick(devicesArray, { title: 'Select the target device', canPickMany: false }).then((value) => {
             if (value !== undefined) {
                 let statusBarMessage = vscode.window.setStatusBarMessage('Running the project...');
                 let process = child_process.exec(workspaceUri[0].uri.path + '/kmtr run --target ' + value, { cwd: workspaceUri[0].uri.path });
