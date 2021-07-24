859e9956c43d7849506f875dbbf5d9f0ad14b374
ggoraa <ggoraa1029@gmail.com>
2021-07-24 11:23:12 +0300

    started implementing the main logic for running commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,32 @@
 import * as vscode from 'vscode';
+import * as bombadil from '@sgarciac/bombadil';
+import * as fs from 'fs';
+import * as child_process from 'child_process';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Kumitateru is now active!');
 
 	context.subscriptions.push(vscode.commands.registerCommand('kumitateru.buildProject', () => {
 		// vscode.window.showInformationMessage('Hello World from Kumitateru!');
-		vscode.window.showQuickPick(["item1", "item2"]);
+		let workspaceUri = vscode.workspace.workspaceFolders;
+		if (workspaceUri === undefined) {
+			vscode.window.showErrorMessage('No workspaces were found');
+		} else {
+			let rawConfig = fs.readFileSync(workspaceUri[0].uri.path + '/kumitateru.toml', 'utf-8');
+			let reader = new bombadil.TomlReader;
+			reader.readToml(rawConfig);
+			vscode.window.showQuickPick(reader.result["package_meta"]["devices"], { title: 'Select the target device', canPickMany: false }).then((value) => {
+				let statusBarMessage = vscode.window.setStatusBarMessage('Building the project...');
+				let process = child_process.exec(workspaceUri[0].uri.path + '/kmtr build --target ' + value, { cwd: workspaceUri[0].uri.path });
+				process.on('exit', (code) => {
+					if (code !== 0) {
+						statusBarMessage.dispose();
+					} else {
+						statusBarMessage.dispose();
+					}
+				});
+			});
+		}
 	}));
 	context.subscriptions.push(vscode.commands.registerCommand("kumitateru.runProject", () => {
 		console.log("Run");
