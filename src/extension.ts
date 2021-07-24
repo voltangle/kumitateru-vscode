98edc219746df1d6e66d3918f3a2dcb7cedcdacb
ggoraa <ggoraa1029@gmail.com>
2021-07-24 12:38:08 +0300

    implemented run command without a log, it requires integration with the build system itself

--- src/extension.ts
+++ src/extension.ts
@@ -29,7 +29,26 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	}));
 	context.subscriptions.push(vscode.commands.registerCommand("kumitateru.runProject", () => {
-		console.log("Run");
+		let workspaceUri = vscode.workspace.workspaceFolders;
+		if (workspaceUri === undefined) {
+			vscode.window.showErrorMessage('No workspaces were found, can\'t execute this command.');
+		} else {
+			let rawConfig = fs.readFileSync(workspaceUri[0].uri.path + '/kumitateru.toml', 'utf-8');
+			let reader = new bombadil.TomlReader;
+			reader.readToml(rawConfig);
+			vscode.window.showQuickPick(reader.result["package_meta"]["devices"], { title: 'Select the target device', canPickMany: false }).then((value) => {
+				let statusBarMessage = vscode.window.setStatusBarMessage('Running the project...');
+				let process = child_process.exec(workspaceUri[0].uri.path + '/kmtr run --target ' + value, { cwd: workspaceUri[0].uri.path });
+				process.on('exit', (code) => {
+					if (code !== 0) {
+						vscode.window.showErrorMessage('Build finished with errors. Rerun from the command line for details.');
+					} else {
+						vscode.window.showInformationMessage('Run finished successfully.');
+					}
+					statusBarMessage.dispose();
+				});
+			});
+		}
 	}));
 }
 
