c6e2bc330ffc64729779c15e069d124c334bc11c
ggoraa <ggoraa1029@gmail.com>
2021-07-27 00:02:49 +0300

    Complete rebuild of how building will be done

--- src/buildApi.ts
+++ src/buildApi.ts
@@ -3,15 +3,16 @@ import * as child_process from 'child_process';
 import { getParsedConfig } from './configParser';
 import * as process from 'process';
 
-export function buildProject() {
+export function runKmtrFunction(action: string) {
     let workspaceUri = vscode.workspace.workspaceFolders;
+
     if (workspaceUri === undefined) {
         vscode.window.showErrorMessage('No workspaces were found, can\'t execute this command.');
     } else {
         let devicesArray = getParsedConfig(workspaceUri[0].uri.path + '/package.toml')["package_meta"]["devices"];
-        vscode.window.showQuickPick(devicesArray, { title: 'Select the target device', canPickMany: false }).then((value) => {
+        let buildFunction = (value) => {
             if (value !== undefined) {
-                let statusBarMessage = vscode.window.setStatusBarMessage('Building the project...');
+                let statusBarMessage = vscode.window.setStatusBarMessage(action === 'run' ? 'Running the project...' : 'Building the project...');
                 let writeEmitter = new vscode.EventEmitter<string>();
                 let runProcess: child_process.ChildProcess;
                 const pty: vscode.Pseudoterminal = {
@@ -24,8 +25,25 @@ export function buildProject() {
                     handleInput: async (data) => {
                         // eslint-disable-next-line @typescript-eslint/naming-convention
                         let env = Object.assign({}, { 'KMTR_IDE_SILENT': 'true' }, process.env);
-                        
-                        runProcess = child_process.exec(workspaceUri[0].uri.path + '/kmtr build --target ' + value, {
+
+                        let runCommand: string;
+                        switch (action) {
+                            case 'run':
+                                runCommand = workspaceUri[0].uri.path + '/kmtr ' + action + ' --target ' + value;
+                                break;
+                            
+                            case 'build':
+                                runCommand = workspaceUri[0].uri.path + '/kmtr ' + action + ' --target ' + value;
+                                break;
+
+                            case 'package':
+                                runCommand = workspaceUri[0].uri.path + '/kmtr build';
+                                break;
+                            default:
+                                break;
+                        }
+
+                        runProcess = child_process.exec(runCommand, {
                             cwd: workspaceUri[0].uri.path,
                             env
                         });
@@ -51,12 +69,12 @@ export function buildProject() {
                         });
                         runProcess.on('exit', (code) => {
                             if (code === null) {
-                                vscode.window.showInformationMessage('Build not completed.');
+                                vscode.window.showInformationMessage(action === 'run' ? 'Run finished.' : 'Build finished');
                             } else {
                                 if (code !== 0) {
-                                    vscode.window.showErrorMessage('Build finished with errors.');
+                                    vscode.window.showErrorMessage(action === 'run' ? 'Run finished with errors.' : 'Build finished with errors.');
                                 } else {
-                                    vscode.window.showInformationMessage('Build finished successfully.');
+                                    vscode.window.showInformationMessage(action === 'run' ? 'Run finished successfully.' : 'Build finished successfully.');
                                 }
                             }
                             statusBarMessage.dispose();
@@ -64,84 +82,17 @@ export function buildProject() {
                     }
                 };
                 let terminal = vscode.window.createTerminal({
-                    name: 'Kumitateru Build',
+                    name: action === 'run' ? 'Kumitateru Run' : 'Kumitateru Build',
                     pty
                 });
                 terminal.sendText('run');
                 terminal.show();
             }
-        });
-    }
-}
-
-export function runProject() {    
-    let workspaceUri = vscode.workspace.workspaceFolders;
-
-    if (workspaceUri === undefined) {
-        vscode.window.showErrorMessage('No workspaces were found, can\'t execute this command.');
-    } else {        
-        let devicesArray = getParsedConfig(workspaceUri[0].uri.path + '/package.toml')["package_meta"]["devices"];
-        vscode.window.showQuickPick(devicesArray, { title: 'Select the target device', canPickMany: false }).then((value) => {
-            if (value !== undefined) {
-                let statusBarMessage = vscode.window.setStatusBarMessage('Running the project...');
-                let writeEmitter = new vscode.EventEmitter<string>();
-                let runProcess: child_process.ChildProcess;
-                const pty: vscode.Pseudoterminal = {
-                    onDidWrite: writeEmitter.event,
-                    open: () => {
-                    },
-                    close: () => {
-                        runProcess.kill();
-                    },
-                    handleInput: async (data) => {
-                        // eslint-disable-next-line @typescript-eslint/naming-convention
-                        let env = Object.assign({}, { 'KMTR_IDE_SILENT': 'true' }, process.env);
-                        
-                        runProcess = child_process.exec(workspaceUri[0].uri.path + '/kmtr run --target ' + value, {
-                            cwd: workspaceUri[0].uri.path,
-                            env
-                        });
-                        runProcess.stdout.on('data', (data: string) => {
-                            let readyData = data.split('\n');
-                            readyData.forEach((element) => {
-                                element = element + '\r';
-                            });
-                            readyData.forEach((element) => {
-                                writeEmitter.fire(element + '\n');
-                                writeEmitter.fire('\r');
-                            });
-                        });
-                        runProcess.stderr.on('data', (data) => {
-                            let readyData = data.split('\n');
-                            readyData.forEach((element) => {
-                                element = element + '\r';
-                            });
-                            readyData.forEach((element) => {
-                                writeEmitter.fire(element + '\n');
-                                writeEmitter.fire('\r');
-                            });
-                        });
-                        runProcess.on('exit', (code) => {
-                            if (code === null) {
-                                vscode.window.showInformationMessage('Run finished.');
-                            } else {
-                                if (code !== 0) {
-                                    vscode.window.showErrorMessage('Run finished with errors.');
-                                } else {
-                                    vscode.window.showInformationMessage('Run finished successfully.');
-                                }
-                            }
-                            statusBarMessage.dispose();
-                        });
-                    }
-                };
-                let terminal = vscode.window.createTerminal({
-                    name: 'Kumitateru Run',
-                    pty
-                });
-                terminal.sendText('run');
-                terminal.show();
-            }
-        });
+        };
+        if (action === 'package') {
+            buildFunction('package');
+        } else {
+            vscode.window.showQuickPick(devicesArray, { title: 'Select the target device', canPickMany: false }).then((value) => buildFunction(value));
+        }
     }
 }
