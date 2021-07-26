2d928079559089437345f527914641eba8be9861
ggoraa <ggoraa1029@gmail.com>
2021-07-26 13:48:44 +0300

    Updated run UI Now it will display run logs inside a custom integrated terminal

--- src/buildApi.ts
+++ src/buildApi.ts
@@ -3,13 +3,14 @@ import * as bombadil from '@sgarciac/bombadil';
 import * as fs from 'fs';
 import * as child_process from 'child_process';
 import { getParsedConfig } from './configParser';
+import * as process from 'process';
 
 export function buildProject() {
     let workspaceUri = vscode.workspace.workspaceFolders;
     if (workspaceUri === undefined) {
         vscode.window.showErrorMessage('No workspaces were found, can\'t execute this command.');
     } else {
-        let devicesArray = getParsedConfig(fs.readFileSync(workspaceUri[0].uri.path + '/kumitateru.toml', 'utf-8'))["package_meta"]["devices"];
+        let devicesArray = getParsedConfig(workspaceUri[0].uri.path + '/package.toml')["package_meta"]["devices"];
         vscode.window.showQuickPick(devicesArray, { title: 'Select the target device', canPickMany: false }).then((value) => {
             if (value !== undefined) {
                 let statusBarMessage = vscode.window.setStatusBarMessage('Building the project...');
@@ -27,24 +28,73 @@ export function buildProject() {
     }
 }
 
-export function runProject() {
+export function runProject() {    
     let workspaceUri = vscode.workspace.workspaceFolders;
+
     if (workspaceUri === undefined) {
         vscode.window.showErrorMessage('No workspaces were found, can\'t execute this command.');
-    } else {
-        let devicesArray = getParsedConfig(fs.readFileSync(workspaceUri[0].uri.path + '/kumitateru.toml', 'utf-8'))["package_meta"]["devices"];
+    } else {        
+        let devicesArray = getParsedConfig(workspaceUri[0].uri.path + '/package.toml')["package_meta"]["devices"];
         vscode.window.showQuickPick(devicesArray, { title: 'Select the target device', canPickMany: false }).then((value) => {
             if (value !== undefined) {
                 let statusBarMessage = vscode.window.setStatusBarMessage('Running the project...');
-                let process = child_process.exec(workspaceUri[0].uri.path + '/kmtr run --target ' + value, { cwd: workspaceUri[0].uri.path });
-                process.on('exit', (code) => {
-                    if (code !== 0) {
-                        vscode.window.showErrorMessage('Build finished with errors. Rerun from the command line for details.');
-                    } else {
-                        vscode.window.showInformationMessage('Run finished successfully.');
+                let writeEmitter = new vscode.EventEmitter<string>();
+                let runProcess: child_process.ChildProcess;
+                const pty: vscode.Pseudoterminal = {
+                    onDidWrite: writeEmitter.event,
+                    open: () => {
+                    },
+                    close: () => {
+                        runProcess.kill();
+                    },
+                    handleInput: (data) => {
+                        // eslint-disable-next-line @typescript-eslint/naming-convention
+                        let env = Object.assign({}, { 'KMTR_IDE_SILENT': 'true' }, process.env);
+                        
+                        runProcess = child_process.exec(workspaceUri[0].uri.path + '/kmtr run --target ' + value, {
+                            cwd: workspaceUri[0].uri.path,
+                            env
+                        });
+                        runProcess.stdout.on('data', (data: string) => {
+                            let readyData = data.split('\n');
+                            readyData.forEach((element) => {
+                                element = element + '\r';
+                            });
+                            readyData.forEach((element) => {
+                                writeEmitter.fire(element + '\n');
+                                writeEmitter.fire('\r');
+                            });
+                        });
+                        runProcess.stderr.on('data', (data) => {
+                            let readyData = data.split('\n');
+                            readyData.forEach((element) => {
+                                element = element + '\r';
+                            });
+                            readyData.forEach((element) => {
+                                writeEmitter.fire(element + '\n');
+                                writeEmitter.fire('\r');
+                            });
+                        });
+                        runProcess.on('exit', (code) => {
+                            if (code === null) {
+                                vscode.window.showInformationMessage('Run finished.');
+                            } else {
+                                if (code !== 0) {
+                                    vscode.window.showErrorMessage('Build finished with errors.');
+                                } else {
+                                    vscode.window.showInformationMessage('Run finished successfully.');
+                                }
+                            }
+                            statusBarMessage.dispose();
+                        });
                     }
-                    statusBarMessage.dispose();
+                };
+                let terminal = vscode.window.createTerminal({
+                    name: 'Kumitateru Run',
+                    pty
                 });
+                terminal.sendText('run');
+                terminal.show();
             }
         });
     }
