a681e6a82896a9679c08a0071634216a1b8a8fc0
ggoraa <ggoraa1029@gmail.com>
2021-07-26 13:59:44 +0300

    Implemented the same thing for building

--- src/buildApi.ts
+++ src/buildApi.ts
@@ -1,6 +1,4 @@
 import * as vscode from 'vscode';
-import * as bombadil from '@sgarciac/bombadil';
-import * as fs from 'fs';
 import * as child_process from 'child_process';
 import { getParsedConfig } from './configParser';
 import * as process from 'process';
@@ -14,15 +12,63 @@ export function buildProject() {
         vscode.window.showQuickPick(devicesArray, { title: 'Select the target device', canPickMany: false }).then((value) => {
             if (value !== undefined) {
                 let statusBarMessage = vscode.window.setStatusBarMessage('Building the project...');
-                let process = child_process.exec(workspaceUri[0].uri.path + '/kmtr build --target ' + value, { cwd: workspaceUri[0].uri.path });
-                process.on('exit', (code) => {
-                    if (code !== 0) {
-                        vscode.window.showErrorMessage('Build finished with errors. Rerun from the command line for details.');
-                    } else {
-                        vscode.window.showInformationMessage('Build finished successfully.');
+                let writeEmitter = new vscode.EventEmitter<string>();
+                let runProcess: child_process.ChildProcess;
+                const pty: vscode.Pseudoterminal = {
+                    onDidWrite: writeEmitter.event,
+                    open: () => {
+                    },
+                    close: () => {
+                        runProcess.kill();
+                    },
+                    handleInput: (data) => {
+                        // eslint-disable-next-line @typescript-eslint/naming-convention
+                        let env = Object.assign({}, { 'KMTR_IDE_SILENT': 'true' }, process.env);
+                        
+                        runProcess = child_process.exec(workspaceUri[0].uri.path + '/kmtr build --target ' + value, {
+                            cwd: workspaceUri[0].uri.path,
+                            env
+                        });
+                        runProcess.stdout.on('data', (data: string) => {
+                            let readyData = data.split('\n');
+                            readyData.forEach((element) => {
+                                element = element + '\r';
+                            });
+                            readyData.forEach((element) => {
+                                writeEmitter.fire(element + '\n');
+                                writeEmitter.fire('\r');
+                            });
+                        });
+                        runProcess.stderr.on('data', (data) => {
+                            let readyData = data.split('\n');
+                            readyData.forEach((element) => {
+                                element = element + '\r';
+                            });
+                            readyData.forEach((element) => {
+                                writeEmitter.fire(element + '\n');
+                                writeEmitter.fire('\r');
+                            });
+                        });
+                        runProcess.on('exit', (code) => {
+                            if (code === null) {
+                                vscode.window.showInformationMessage('Build not completed.');
+                            } else {
+                                if (code !== 0) {
+                                    vscode.window.showErrorMessage('Build finished with errors.');
+                                } else {
+                                    vscode.window.showInformationMessage('Build finished successfully.');
+                                }
+                            }
+                            statusBarMessage.dispose();
+                        });
                     }
-                    statusBarMessage.dispose();
+                };
+                let terminal = vscode.window.createTerminal({
+                    name: 'Kumitateru Build',
+                    pty
                 });
+                terminal.sendText('run');
+                terminal.show();
             }
         });
     }
@@ -80,7 +126,7 @@ export function runProject() {
                                 vscode.window.showInformationMessage('Run finished.');
                             } else {
                                 if (code !== 0) {
-                                    vscode.window.showErrorMessage('Build finished with errors.');
+                                    vscode.window.showErrorMessage('Run finished with errors.');
                                 } else {
                                     vscode.window.showInformationMessage('Run finished successfully.');
                                 }
